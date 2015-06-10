e5ad35c95a434e8019645292e9dd9bd72a56f826
crystal <gisele6@126.com>
2015-06-10 16:51:09 +0800

    消费记录-添加加载更多按钮

--- app/scripts/controllers/user-center/investment-record.js
+++ app/scripts/controllers/user-center/investment-record.js
@@ -10,10 +10,11 @@
 angular.module('o2oWechatIou')
   .controller('InvestmentRecordCtrl', ['$scope', '$rootScope', '$state', 'IouUser', 'restmod', 'DEFAULT_DOMAIN', 'config', function ($scope, $rootScope, $state, IouUser, restmod, DEFAULT_DOMAIN, config) {
     
-    var checkModel = restmod.model(DEFAULT_DOMAIN + '/users');
-    checkModel.$find('checkSession').$then(function(response) {
+    // var checkModel = restmod.model(DEFAULT_DOMAIN + '/users');
+    IouUser.$find('checkSession').$then(function(response) {
       if (response.user) {
         $rootScope.userInfo = response.user;
+        //获取账户信息
         IouUser.$find($rootScope.userInfo.id + '/account').$then(function(response) {
           if (response.$status === 'ok') {
             // 获取用户购买记录列表
@@ -48,7 +49,7 @@ angular.module('o2oWechatIou')
          
         //用户未登录状态
       } else if (!response.user) {
-        // window.location.href = config.weixin_redirect;
+        window.location.href = config.weixin_redirect;
       } else if(response.ret === -1) {
         // $rootScope.isLogged = false;
         $rootScope.userInfo = null;
