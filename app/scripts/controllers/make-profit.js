6fbea1a136967374c2d5896305eda425b89829f8
zhangfeng <zhangfeng@hoolai.com>
2015-06-19 17:49:26 +0800

    调整为快速充值接口

--- app/scripts/controllers/make-profit.js
+++ app/scripts/controllers/make-profit.js
@@ -71,42 +71,11 @@ angular.module('o2oWechatIou')
     $scope.toInvest = function(simpleFundsProject) {
       if (!$scope.investmentFlag) {
         $scope.investmentFlag = true;
-        IouUser.$find('checkSession').$then(function(response) {
-          if (response.user) {
-            $rootScope.userInfo = response.user;
-            // restmod.model(DEFAULT_DOMAIN + '/fundsProjects/' + simpleFundsProject.number + '/users/6/investment').$create({
-            restmod.model(DEFAULT_DOMAIN + '/fundsProjects/' + simpleFundsProject.number + '/users/' + $rootScope.userInfo.id + '/investment').$create({
-              amount: simpleFundsProject.investAmount,
-              projectId: simpleFundsProject.id,
-              isRepeat: 2
-            }).$then(function(response) {
-              if (response.ret !== -1) {
-                if (response.number) {
-                  // restmod.model(DEFAULT_DOMAIN + '/orders/' + response.number + '/users/6/payment').$create({
-                  restmod.model(DEFAULT_DOMAIN + '/orders/' + response.number + '/users/' + $rootScope.userInfo.id + '/payment').$create({
-                      payType: 2
-                    }).$then(function(response) {
-                    if (response.ret !== -1) {
-                      var merchantaccount = response.merchantaccount;
-                      var encryptkey = response.encryptkey;
-                      var data = response.data;
-                      var _f = newForm(); //创建一个form表单
-                      createElements(_f, 'merchantaccount', merchantaccount); //创建form中的input对象
-                      createElements(_f, 'encryptkey', encryptkey); //创建form中的input对象
-                      createElements(_f, 'data', data); //创建form中的input对象
-                      _f.action = 'https://ok.yeepay.com/paymobile/api/pay/request'; //form提交地址
-                      _f.submit(); //提交
-                    } else {
-                      $scope.msg = response.msg;
-                    }
-                  });
-                }
-
-              } else {
-                $scope.msg = response.msg;
-              }
-            });
-          }
+        IouUser.$find($rootScope.userInfo.id + '/quickRecharge' + '?amount=' + simpleFundsProject.investAmount)
+          .$then(function(response) {
+            if (response.ret === -1){
+              $scope.msg = response.msg;
+            }
         });
       }
 
